aebafa6282a104ba338ccaff92b94daeb7a092a5
Mohammed <m7amd.s3ad@hotmail.com>
2019-02-12 13:58:16 +0300

    Mohammed Aljumiah , Rasha

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=50)
+    contnet = models.CharField(max_length=100)
+    last_update = models.DateField(auto_now_add = True)
+
+    def get_absolute_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.id})
